2cb643a3677c81e0e3a894497bdaec2993f973aa
Visar Zejnullahu <visar.zejnullahu@gmail.com>
2020-12-02 11:16:50 +0100

    Oneliners for Day 1

--- src/main/scala/advent/Day1.scala
+++ src/main/scala/advent/Day1.scala
@@ -4,21 +4,8 @@ import scala.io.Source
 
 object Day1 extends App {
   val filename = "day1.in"
-  val lines = Source.fromFile(filename).getLines.toList.map(_.toInt).sorted
+  val lines    = Source.fromFile(filename).getLines.toList.map(_.toInt).sorted
 
-  val partOne = (for {
-    x <- lines
-    if x < 2020 - x && lines.contains(2020 - x)
-  } yield x * (2020 - x)).head
-
-  println(partOne)
-
-  val partTwo = (for {
-    x <- lines
-    y <- lines
-    z <- lines
-    if x < y && y < z && x + y + z == 2020
-  } yield x * y * z).head
-
-  println(partTwo)
+  println(lines.combinations(2).filter(_.sum == 2020).toList.head.product)
+  println(lines.combinations(3).filter(_.sum == 2020).toList.head.product)
 }
